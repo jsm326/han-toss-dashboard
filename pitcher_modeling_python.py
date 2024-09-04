807711688e8f7cffb83c2d31a44a2aab5520768b
jsm326 <jsm3260@daum.net>
2024-09-04 18:37:57 +0900

    ad

--- pitcher_modeling_python.py
+++ pitcher_modeling_python.py
@@ -68,7 +68,7 @@ data = pd.read_csv('./data/pitcher_data.csv')
 df_input = data[['ERA', 'QS_G', 'SO', 'WAR_x', 'W', 'IP', 'K_BB', 'exp_QS','SO_G', 'QS', 'NP', 'RA_9', '연차', 'TBF', '현재연봉', 'WHIP', 'K-BB', 'NP/IP']]
 df_target = data['salary_cluster']
 
-parameter = {'max_depth' : 3, 'eta' : 0.006, 'objective' : 'multi:softmax', 'eval_metrics' : 'merror', 'num_class' : 4}
+parameter = {'max_depth' : 3, 'eta' : 0.006, 'objective' : 'multi:softmax', 'eval_metric' : 'merror', 'num_class' : 4}
 
 from pitcher_modeling_python import xgb_c_model
 x_model = xgb_c_model(df_input, df_target)
